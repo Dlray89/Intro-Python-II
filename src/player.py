402995c7b59d44f9bc23b3c7a5392f6046f94f1e
DAP <dlrayjr89@gmail.com>
2020-06-03 22:58:27 +0000

    player builded out

--- src/player.py
+++ src/player.py
@@ -1,2 +1,29 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+        def __str__(self):
+            output = f'{self.name}\n'
+            for idx, description in enumerate(self.description):
+                output += " " + str(idx+1) + '. ' + description + '\n'
+            return output
+
+        
+
+#         class Store:
+#     def __init__(self, name, categories):
+#         self.name = name
+#         self.categories = categories
+#     def __str__(self):
+#         output = f"{self.name}\n"
+#         for idx, category in enumerate(self.categories):
+#             output += "  " + str(idx+1) + ". " + category + "\n"
+#         return output
+# my_store = Store("The Dugout", ["Running", "Baseball", "Basketball"])
+# print(my_store)
+# selection = input("Select the number of a department:")
+# print("The user selected " + str(selection))
